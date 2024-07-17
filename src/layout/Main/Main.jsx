d27cc85996918c7acc0fdf3f5f702ff8b14b0b56
TCOD <tcoddev@gmail.com>
2024-07-18 01:45:49 +0330

    create Services.jsx

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,9 +1,11 @@
 import Landing from "../../components/Landing/Landing";
+import Services from "../../components/Services/Services";
 
 export default function Main() {
   return (
     <>
       <Landing />
+      <Services />
     </>
   );
 }
